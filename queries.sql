f206c7d8b9d0bf912f17957e016fb1a00ca60bbd
Igor Yermak <igor.yermak@gmail.com>
2018-06-08 14:54:21 -0400

    10 different messages

--- queries.sql
+++ queries.sql
@@ -41,6 +41,18 @@ INSERT INTO user (name) VALUES ("Chris");
 INSERT INTO channel (name) VALUES ("#general");
 INSERT INTO channel (name) VALUES ("#random");
 
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Alice", 1, 1, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Bob", 2, 1, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Chris", 3, 2, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Well this is fun", 1, 1, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("I think so too", 2, 1, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Hey Chris", 1, 2, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Hi Alice", 3, 2, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Isn't this wonderful", 1, 2, CURRENT_TIMESTAMP);
+INSERT INTO message(content, user_id, channel_id, post_time) VALUES ("Sure Alice", 3, 2, CURRENT_TIMESTAMP);
+
+
+
 -- 1. List all organization `name`s.--
 SELECT name FROM organization;
 --   2. List all channel `name`s.--
