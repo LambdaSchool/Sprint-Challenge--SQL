7759f224837dfe3be17181768d25f19a81901fcb
Peter Gray <peter@petergraycreative.com>
2018-06-08 10:40:36 -0500

    Initial Commit
    Began building out tables

---
+++ queries.sql
@@ -0,0 +1,21 @@
+DROP TABLE [IF EXISTS]
+organization;
+DROP TABLE [IF EXISTS]
+channel;
+DROP TABLE [IF EXISTS]
+user;
+DROP TABLE [IF EXISTS]
+message;
+CREATE TABLE organization (
+  org_id int PRIMARY KEY AUTO_INCREMENT,
+  
+);
+CREATE TABLE channel (
+
+);
+CREATE TABLE user (
+
+);
+CREATE TABLE message (
+
+);
