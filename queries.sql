418bf7b8f80e8c4af81a06c740a38f63d882b79c
JohnJohnX4 <johnjohnx4@gmail.com>
2018-06-08 13:00:59 -0400

    wokring on SELECT statements

--- queries.sql
+++ queries.sql
@@ -1,101 +1,96 @@
-DROP TABLE IF EXISTS organization
-DROP TABLE IF EXISTS channel
-DROP TABLE IF EXISTS user
-DROP TABLE IF EXISTS message
+DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS message;
 
 CREATE TABLE organization(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(32) NOT NULL
-)
+);
+
 CREATE TABLE channel(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64) NOT NULL,
     organization_id INTEGER,
     FOREIGN KEY(organization_id) REFERENCES organization(id)
-)
+);
+
 CREATE TABLE user(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(16) NOT NULL
-)
+);
+
 CREATE TABLE message(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
     channel_id INTEGER,
     content TEXT,
     post_time DATETIME NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    FOREIGN KEY(user_id) REFERENCES user(id),
     FOREIGN KEY(channel_id) REFERENCES channel(id)
-)
+);
 
 INSERT INTO organization (name) VALUES ("Lambda School");
+
+INSERT INTO channel (name, organization_id) VALUES ("#general", 1);
+INSERT INTO channel (name, organization_id) VALUES ("#random", 1);
 
 INSERT INTO user (name) VALUES ("Alice");
 INSERT INTO user (name) VALUES ("Bob");
 INSERT INTO user (name) VALUES ("Chris");
 
-INSERT INTO channel (name) VALUES ("#general");
-INSERT INTO channel (name) VALUES ("#random");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (1, 1, CURRENT_TIMESTAMP, "Hello!");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (2, 1, CURRENT_TIMESTAMP, "Hey, good morning");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (1, 1, CURRENT_TIMESTAMP, "What are we doing today?");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (2, 1, CURRENT_TIMESTAMP, "I think we are free now..");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (1, 1, CURRENT_TIMESTAMP, "No, we still have Labs!");
 
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
-INSERT INTO message (user_id, content) VALUES (1, "text");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (1, 2, CURRENT_TIMESTAMP, "RIP Anthony Bourdain");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (3, 2, CURRENT_TIMESTAMP, "Oh wow, I didnt even know he passed away!");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (1, 2, CURRENT_TIMESTAMP, "Yeah it's so crazy!");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (3, 2, CURRENT_TIMESTAMP, "Wow, I loved hi shows");
+INSERT INTO message (user_id, channel_id, post_time, content) VALUES (1, 2, CURRENT_TIMESTAMP, "Me too man, me too..");
 
+SELECT " ";
 
--- 10 messages (at least one per user, and at least one per channel).
--- Alice should be in #general and #random.
--- Bob should be in #general.
--- Chris should be in #random.
--- Write SELECT queries to:
+SELECT "List all organization names.";
+SELECT organization.name from organization;
+SELECT " ";
 
--- For these INSERTs, it is NOT OK to refer to users, channels, and organization by their ids. You must join in those cases.
+SELECT "List all channel names.";
+SELECT channel.name from channel;
+SELECT " ";
 
--- List all organization names.
+SELECT "List all channels in a specific organization by organization name.";
+SELECT channel.name, organization.name
+FROM channel, organization
+WHERE channel.organization_id = organization.id
+AND organization.name = "Lambda School";
+SELECT " ";
 
--- List all channel names.
+SELECT "List all messages in a specific channel by channel name #general in order of post_time, descending.";
+SELECT channel.name, user.name, message.content, message.post_time
+FROM message, user, channel
+WHERE message.user_id = user.id 
+AND message.channel_id = channel.id
+AND channel.name = "#general"
+ORDER BY message.post_time DESC;
+SELECT " ";
 
--- List all channels in a specific organization by organization name.
+SELECT "List all channels to which user Alice belongs.";
 
--- List all messages in a specific channel by channel name #general in order of post_time, descending. (Hint: ORDER BY. Because your INSERTs might have all taken place at the exact same time, this might not return meaningful results. But humor us with the ORDER BY anyway.)
+SELECT " ";
 
--- List all channels to which user Alice belongs.
+SELECT "List all users that belong to channel #general.";
 
--- List all users that belong to channel #general.
+SELECT " ";
 
--- List all messages in all channels by user Alice.
+SELECT " List all messages in all channels by user Alice.";
 
--- List all messages in #random by user Bob.
+SELECT " ";
 
--- List the count of messages across all channels per user. (Hint: COUNT, GROUP BY.)
+SELECT " List all messages in #random by user Bob.";
 
--- The title of the user's name column should be User Name and the title of the count column should be Message Count. (The SQLite commands .mode column and .header on might be useful here.)
+SELECT " ";
 
--- The user names should be listed in reverse alphabetical order.
-
--- Example:
-
--- User Name   Message Count
--- ----------  -------------
--- Chris       4
--- Bob         3
--- Alice       3
--- [Stretch!] List the count of messages per user per channel.
-
--- Example:
-
--- User        Channel     Message Count
--- ----------  ----------  -------------
--- Alice       #general    1
--- Bob         #general    1
--- Chris       #general    2
--- Alice       #random     2
--- Bob         #random     2
--- Chris       #random     2
--- What SQL keywords or concept would you use if you wanted to automatically delete all messages by a user if that user were deleted from the user table?
-
+SELECT " List the count of messages across all channels per user. (Hint: COUNT, GROUP BY.)";
