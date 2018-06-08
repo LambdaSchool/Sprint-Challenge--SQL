9d7afe4d232b1bf75db64e0a72dda919becb0bd6
Cliff Kang <inlovewithhim@gmail.com>
2018-06-08 09:41:21 -0700

    insert queries done

--- queries.sql
+++ queries.sql
@@ -7,8 +7,7 @@ DROP TABLE IF EXISTS message;
 
 CREATE TABLE organization (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(128) NOT NULL,
-    content VARCHAR(1024)
+    name VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE channel (
@@ -17,21 +16,54 @@ CREATE TABLE channel (
     org_id INTEGER REFERENCES organization(id)
 );
 
-CREATE TABLE channel_user_message (
-    channel_id INTEGER REFERENCES channel(id),
-    user_id INTEGER REFERENCES user(id),
-    message_id INTEGER REFERENCES message(id)
-);
-
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(128) NOT NULL,
-    channel_id INTEGER REFERENCES channel(id)
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE channel_user (
+    channel_id INTEGER REFERENCES channel(id),
+    user_id INTEGER REFERENCES user(id)
 );
 
 CREATE TABLE message (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     content VARCHAR(1024) NOT NULL,
     post_time DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
-    user_id INTEGER REFERENCES user(id)
+    user_id INTEGER REFERENCES user(id),
+    channel_id INTEGER REFERENCES channel(id)
 );
+
+
+INSERT INTO organization (name) VALUES ("Lambda School");
+INSERT INTO channel (name, org_id) VALUES ("#general", 1);
+INSERT INTO channel (name, org_id) VALUES ("#random", 1);
+INSERT INTO user (name) VALUES ("Alice");
+INSERT INTO user (name) VALUES ("Bob");
+INSERT INTO user (name) VALUES ("Chris");
+
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("hi!", 1, 1);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("hi, Alice!", 2, 1);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("how you doing, Bob?", 1, 1);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("It's too early!", 2, 1);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("hi!", 3, 2);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("hey there, Chris!", 1, 2);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("It's a beautiful day here", 3, 2);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("Oh, where are you?", 1, 2);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("Of course I'm in CA!", 3, 2);
+INSERT INTO message (content, user_id, channel_id)
+    VALUES ("Going to go take a nap!", 2, 1);
+
+INSERT INTO channel_user (channel_id, user_id) VALUES (1, 1);
+INSERT INTO channel_user (channel_id, user_id) VALUES (1, 2);
+INSERT INTO channel_user (channel_id, user_id) VALUES (2, 1);
+INSERT INTO channel_user (channel_id, user_id) VALUES (3, 2);
