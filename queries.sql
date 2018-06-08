7fb0df330fbe46c815682e9d6170f5c739cbb424
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-06-08 13:10:14 -0400

    Inserts Completed
    -Inserted Lambda School into organization
    -Inserted Alice, Bob, Chris into user
    -Inserted general and random into channels
    -Inserted messages from different users in different channels
    -Added Alice to channels general and random
    -Added Bob to channel general
    -Added Chris to channel random

--- queries.sql
+++ queries.sql
@@ -26,3 +26,33 @@ CREATE TABLE channel_user (
     channel_id INTEGER REFERENCES channel(id),
     user_id INTEGER REFERENCES user(id)
 );
+
+INSERT INTO organization (name) VALUES ('Lambda School');
+
+INSERT INTO user (name) VALUES ('Alice');
+INSERT INTO user (name) VALUES ('Bob');
+INSERT INTO user (name) VALUES ('Chris');
+
+INSERT INTO channel (name, organization_id) VALUES ('#general', 1);
+INSERT INTO channel (name, organization_id) VALUES ('#random', 1);
+
+INSERT INTO message (content, user_id, channel_id) VALUES ('How much', 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('wood', 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('coulda', 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('woodchuck', 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('chuck', 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('if a', 2, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('woodchuck', 3, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('could', 3, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('chuck', 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('wood', 3, 2);
+
+-- Alice should be in #general and #random.
+INSERT INTO user_channel (user_id, channel_id) VALUES (1, 1);
+INSERT INTO user_channel (user_id, channel_id) VALUES (1, 2);
+
+-- Bob should be in #general.
+INSERT INTO user_channel (user_id, channel_id) VALUES (2, 1);
+
+-- Chris should be in #random.
+INSERT INTO user_channel (user_id, channel_id) VALUES (3, 2);
