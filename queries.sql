e2a64892999129c8b9d7e95ca720184b0d7b92f6
Igor Yermak <igor.yermak@gmail.com>
2018-06-08 14:23:44 -0400

    table creates

---
+++ queries.sql
@@ -0,0 +1,29 @@
+CREATE TABLE organization(
+  id INTERGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(32) NOT NULL
+
+)
+
+CREATE TABLE channel(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(128) NOT NULL,
+  organization_id INTEGER REFERENCES organization(id)
+)
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(128) NOT NULL,
+)
+
+CREATE TABLE message (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(128) NOT NULL,
+  post_time DATETIME NOT NULL,
+  user_id INTEGER REFERENCES user(id),
+  channel_id INTEGER REFERENCES channel(id)
+)
+
+CREATE TABLE channel_user (
+  channel_id INTEGER REFERENCES channel(id)
+  user_id INTEGER REFERENCES user(id)
+)
