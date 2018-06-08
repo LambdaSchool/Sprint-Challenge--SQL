b0125db394df12fe32ceb10663cabaafa7f7d437
Res Yeg <shaqk702@gmail.com>
2018-06-08 09:27:31 -0700

    added CREATEs

--- queries.sql
+++ queries.sql
@@ -1,3 +1,14 @@
+PRAGMA foreign_keys = ON;
+
+.mode column
+.header on
+
+DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS message;
+
+-- Write CREATE TABLE statements for tables organization, channel, user, and message
 CREATE TABLE organization (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(128) NOT NULL,
@@ -5,16 +16,31 @@ CREATE TABLE organization (
 
 CREATE TABLE channel (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(128) NOT NULL
+    name VARCHAR(128) NOT NULL,
+    organization_id INTEGER REFERENCES organization(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(128) NOT NULL
+    name VARCHAR(128) NOT NULL,
+    channel_id INTEGER REFERENCES channel(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE message (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    post_time ,
-    content VARCHAR(1024) NOT NULL
+    post_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    content VARCHAR(1024) NOT NULL,
+    user_id INTEGER REFERENCES user(id) ON DELETE CASCADE NOT NULL,
+    channel_id INTEGER REFERENCES channel(id) ON DELETE CASCADE NOT NULL
 );
+
+CREATE TABLE user_channel (
+    user_id INTEGER REFERENCES user(id),
+    channel_id INTEGER REFERENCES channel(id)
+);
+
+-- Write INSERT queries to add information to the database.
+
+
+
+-- Write SELECT queries
