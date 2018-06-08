bdacf57f79957ca9f1baef44b09bffd2d855660f
Michael Marshal <mikemarshalman@gmail.com>
2018-06-08 12:58:11 -0500

    inserts added

--- queries.sql
+++ queries.sql
@@ -23,5 +23,29 @@ CREATE TABLE user (
 
 CREATE TABLE message (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  post_time DATETIME default current_time,
+  content TEXT,
+  user_id INTEGER REFERENCES user(id),
+  channel_id INTEGER REFERENCES channel(id),
+  post_time TIMESTAMP NOT NULL DEFAULT(DATETIME()),
 )
+
+CREATE TABLE user_channel (
+  user_id INTEGER REFERENCES user(id),
+  channel_id INTEGER REFERENCES channel(id)
+)
+
+INSERT INTO organization (name) VALUES ("Lambda School");
+
+INSERT INTO channel (name, organization_id) VALUES ("#general", 1);
+INSERT INTO channel (name, organization_id) VALUES ("#random", 1);
+
+INSERT INTO user (name) VALUES ("Mike");
+INSERT INTO user (name) VALUES ("Rob");
+INSERT INTO user (name) VALUES ("Dan");
+
+INSERT INTO message (content, user_id, channel_id) VALUES ('test', 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('test2', 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('test3', 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('test4', 2, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('test5', 3, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('test6', 3, 2);
