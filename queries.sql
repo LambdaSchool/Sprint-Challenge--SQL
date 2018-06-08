b83fdadb5088f8e9c5110662b3a0855b3a42bd6a
Igor Yermak <igor.yermak@gmail.com>
2018-06-08 14:35:07 -0400

    table resets, add content type to message and INSERTS

--- queries.sql
+++ queries.sql
@@ -1,29 +1,42 @@
+DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS message;
+
+
 CREATE TABLE organization(
   id INTERGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(32) NOT NULL
-
-)
+);
 
 CREATE TABLE channel(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(128) NOT NULL,
   organization_id INTEGER REFERENCES organization(id)
-)
+);
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(128) NOT NULL,
-)
+  name VARCHAR(128) NOT NULL
+);
 
 CREATE TABLE message (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(128) NOT NULL,
+  content VARCHAR(128) NOT NULL,
   post_time DATETIME NOT NULL,
   user_id INTEGER REFERENCES user(id),
   channel_id INTEGER REFERENCES channel(id)
-)
+);
 
 CREATE TABLE channel_user (
   channel_id INTEGER REFERENCES channel(id)
   user_id INTEGER REFERENCES user(id)
-)
+);
+
+INSERT INTO organization (name) VALUES ("Lambda School")
+INSERT INTO user (name) VALUES ("Alice")
+INSERT INTO user (name) VALUES ("Bob")
+INSERT INTO user (name) VALUES ("Chris")
+INSERT INTO channel (name) VALUES ("#general")
+INSERT INTO channel (name) VALUES ("#random")
