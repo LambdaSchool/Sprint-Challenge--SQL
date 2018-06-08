36d61fab6c68371209cbb15d369c6ed22a39a27b
Igor Yermak <igor.yermak@gmail.com>
2018-06-08 14:44:52 -0400

    Inserts

--- queries.sql
+++ queries.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS message;
 
 
-CREATE TABLE organization(
+CREATE TABLE organization (
   id INTERGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(32) NOT NULL
+  name VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE channel(
@@ -34,9 +34,19 @@ CREATE TABLE channel_user (
   user_id INTEGER REFERENCES user(id)
 );
 
-INSERT INTO organization (name) VALUES ("Lambda School")
-INSERT INTO user (name) VALUES ("Alice")
-INSERT INTO user (name) VALUES ("Bob")
-INSERT INTO user (name) VALUES ("Chris")
-INSERT INTO channel (name) VALUES ("#general")
-INSERT INTO channel (name) VALUES ("#random")
+INSERT INTO organization (name) VALUES ("Lambda School");
+INSERT INTO user (name) VALUES ("Alice");
+INSERT INTO user (name) VALUES ("Bob");
+INSERT INTO user (name) VALUES ("Chris");
+INSERT INTO channel (name) VALUES ("#general");
+INSERT INTO channel (name) VALUES ("#random");
+
+-- 1. List all organization `name`s.--
+SELECT name FROM organization;
+--   2. List all channel `name`s.--
+SELECT name from channel;
+-- 3. List all channels in a specific organization by organization `name` --
+SELECT channel.name AS "Channel Name", organization.name as "Organization Name" FROM channel JOIN organization;
+
+-- 4. List all messages in a specific channel by channel `name` `#general` in order of `post_time`, descending. --
+
