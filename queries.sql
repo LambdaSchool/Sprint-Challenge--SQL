7c3f0f2e4d9e414916eae06bcac35778188a90bc
Smithface <kylewibblesmith@gmail.com>
2018-06-08 11:41:08 -0400

    added foreign key references and join table
    channel references organization, message references user and channel, user and channel are connected by a join table

--- queries.sql
+++ queries.sql
@@ -8,6 +8,7 @@ CREATE TABLE organization (
 CREATE TABLE channel (
   id INTEGER PRIMARY KEY AUTOINCREMENT,  
   name VARCHAR(128)
+  organization_id INTEGER REFERENCES organization(id) -- A channel can belong to one organization
 );
 
 CREATE TABLE user (
@@ -18,5 +19,15 @@ CREATE TABLE user (
 CREATE TABLE message (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   content TEXT,
-  post_time TIMESTAMP NOT NULL DEFAULT(DATETIME())
+  post_time TIMESTAMP NOT NULL DEFAULT(DATETIME()),
+  user_id INTEGER REFERENCES user(id), --  a user can post messages to a channel
+  channel_id INTEGER REFERENCES channel(id)
+);
+
+
+-- A channel can have many users subscribed.
+-- A user can be subscribed to many channels.
+CREATE TABLE user_channel (
+  user_id INTEGER REFERENCES user(id),
+  channel_id INTEGER REFERENCES channel(id)
 );
