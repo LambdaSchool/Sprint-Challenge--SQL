67a04080e2428640bddb4ad48c475a5d056ee48a
Seth Anthony <steven.magadan@hotmail.com>
2018-06-08 12:39:24 -0400

    create if not exists

---
+++ queries.sql
@@ -0,0 +1,30 @@
+
+
+
+CREATE TABLE IF NOT EXISTS organization (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(40) NOT NULL
+)
+
+
+CREATE TABLE IF NOT EXISTS channel (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL
+    organization_id INTEGER REFERENCES organization(id)
+)
+
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS message (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL,
+    posted datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+
+INSERT INTO organization (name) VALUES ("Lambda School");
+INSERT INTO user (name) VALUES ("Steven");
+INSERT INTO channel (name, organization_id) VALUES ("#cs7career",1);
