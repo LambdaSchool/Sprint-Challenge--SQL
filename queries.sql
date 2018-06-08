e6af741e4b9ef65bedd32fae7882b5c293769133
Dixie Korley <dixiekorley@gmail.com>
2018-06-08 13:09:26 -0400

    Errors after inserting

--- queries.sql
+++ queries.sql
@@ -1,34 +1,32 @@
-PRAGMA foreign_keys = ON;
-
 DROP TABLE IF EXISTS organization;
-DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS channels;
 DROP TABLE IF EXISTS messages;
 
 CREATE TABLE organization (
-    organization_id INTEGER NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     organization_name VARCHAR(128),
-    PRIMARY KEY(ID)
+
 );
 
 CREATE TABLE channels (
-     channels_id INTEGER NOT NULL,
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
      channels_name VARCHAR(128) NOT NULL,
-     PRIMARY KEY(ID)
+ 
 );
 
 CREATE TABLE users (
-     users_id INTEGER NOT NULL,
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
      users_name VARCHAR(128) NOT NULL,
-     PRIMARY KEY(ID)
+ 
 );
 
 CREATE TABLE messages (
-    message_id INTEGER NOT NULL, 
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
     content VARCHAR(1024),
     post_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     users_id INTEGER REFERENCES users(id),
     channels_id INTEGER REFERENCES channels(id)
-    PRIMARY KEY(ID)
+
 );
 
 INSERT INTO organization (name) VALUES ('Lambda School');
@@ -51,3 +49,22 @@ INSERT INTO messages (content, users_id, channels_id) VALUES ('#Good afternoon',
 INSERT INTO messages (content, users_id, channels_id) VALUES ('What are we doign today?', 3, 2);
 INSERT INTO messages (content, users_id, channels_id) VALUES ('Do I need this app?', 3, 2);
 INSERT INTO messages (content, users_id, channels_id) VALUES ('@dave you coming to the meetup?', 1, 2);
+
+-- List all organization names.
+SELECT * FROM organization;
+-- List all channel names.
+SELECT * FROM channels;
+
+-- List all channels in a specific organization by organization name.
+
+-- List all messages in a specific channel by channel name #general in order of post_time, descending. (Hint: ORDER BY. Because your INSERTs might have all taken place at the exact same time, this might not return meaningful results. But humor us with the ORDER BY anyway.)
+
+-- List all channels to which user Alice belongs.
+
+-- List all users that belong to channel #general.
+
+-- List all messages in all channels by user Alice.
+
+-- List all messages in #random by user Bob.
+
+-- List the count of messages across all channels per user. (Hint: COUNT, GROUP BY.)
