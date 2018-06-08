565cf583eadeab78fb1eaf0c1640dfc70d134911
Peter Gray <peter@petergraycreative.com>
2018-06-08 12:13:16 -0500

    Began writing actual queries

--- queries.sql
+++ queries.sql
@@ -1,26 +1,37 @@
-DROP TABLE [IF EXISTS]
-organization;
-DROP TABLE [IF EXISTS]
-channel;
-DROP TABLE [IF EXISTS]
-user;
-DROP TABLE [IF EXISTS]
-message;
+
+DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS channel; 
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS message;
 
 CREATE TABLE organization (
-  org_id int PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255) NOT NULL
 );
 CREATE TABLE channel (
-  channel_id int PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255) NOT NULL
 );
 CREATE TABLE user (
-  user_id int PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255) NOT NULL,
+  org_id INTEGER,
+  FOREIGN KEY(org_id) REFERENCES organization(id)
 );
+
 CREATE TABLE message (
-  message_id int PRIMARY KEY AUTO_INCREMENT,
-  content VARCHAR(255) NOT NULL,
-  post_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content VARCHAR(512) NOT NULL,
+  post_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  channel_id INTEGER,
+  user_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(channel_id) REFERENCES channel(id)
 );
+
+PRAGMA foreign_keys = ON;
+
+INSERT INTO organization (name) VALUES ("Lambda School");
+INSERT INTO user (name) VALUES ("Alice"), ("Bob"), ("Chris");
+INSERT INTO channel (name) VALUES ("#random"), ("#general");
+
