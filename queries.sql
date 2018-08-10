cbb7f9a8e87caeb734406c4fee864f8f473ce0ce
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-10 11:47:52 -0400

    Create tables completed

---
+++ queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE organization (
+    name VARCHAR(128)
+)
+
+CREATE TABLE channel (
+    name VARCHAR(128)
+)
+
+CREATE TABLE user (
+    name VARCHAR(128)
+)
+
+CREATE TABLE message (
+    post_time DATETIME,
+    content VARCHAR(128)
+)
