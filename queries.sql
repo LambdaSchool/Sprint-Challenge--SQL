40c70b3fefd48960ff330e35b33ac7e252506741
Velsu <riftmage50@gmail.com>
2018-08-10 17:36:23 +0200

    Completed inserts and selects

--- queries.sql
+++ queries.sql
@@ -1,22 +1,23 @@
 pragma foreign_keys = on; -- sqlite only!
 
+
 create table organization (
   id integer primary key autoincrement,
   name varchar(128) not null unique
-)
+);
 
 create table channel (
   id integer primary key autoincrement,
-  name varchar(28) not null uniqe,
+  name varchar(28) not null unique,
   organization_id integer references organization(id)
   
-)
+);
 
 create table user (
   id integer primary key autoincrement,
-  name varchar(38) not null unique,
+  name varchar(38) not null unique
   
-)
+);
 
 create table message (
   id integer primary key autoincrement,
@@ -24,9 +25,72 @@ create table message (
   post_time timestamp not null default(datetime()),
   user_id integer references user(id),
   channel_id integer references channel(id)
-)
+);
 
 create table user_subs (
   user_id integer references user(id),
   channel_id integer references channel(id)
-)
+);
+
+insert into organization (name) values ("Lambda School");
+
+insert into user ( name ) values ("Alice"), ("Bob"), ("Chris");
+
+insert into channel (name, organization_id) values ("#general", 1), ("#random", 1);
+
+insert into message (content, channel_id, user_id) values ("i like potato", 1, 1);
+insert into message (content, channel_id, user_id) values ("rly?", 1, 2);
+insert into message (content, channel_id, user_id) values ("ya rly", 1, 1);
+insert into message (content, channel_id, user_id) values ("anyone wanna play wow.", 2, 3);
+insert into message (content, channel_id, user_id) values ("Why would I play games.", 2, 1);
+insert into message (content, channel_id, user_id) values ("Always good to chill", 2, 3);
+insert into message (content, channel_id, user_id) values ("Better learn js", 2, 1);
+insert into message (content, channel_id, user_id) values ("hello peeps", 1, 2);
+insert into message (content, channel_id, user_id) values ("yo whats up", 1, 1);
+insert into message (content, channel_id, user_id) values ("i wanna play games", 2, 3);
+
+insert into user_subs (user_id, channel_id) values (1, 1);
+insert into user_subs (user_id, channel_id) values (1, 2);
+insert into user_subs (user_id, channel_id) values (2, 1);
+insert into user_subs (user_id, channel_id) values (3, 2);
+
+select name from organization;
+
+select name from channel;
+
+
+select channel.name as "ls_channels", organization.name as "organization" from channel, organization
+where channel.organization_id = organization.id
+and organization.name = "Lambda School";
+
+
+select message.content from message, channel
+where message.channel_id = channel.id
+and channel.name = "#general"
+order by post_time desc;
+
+select channel.name as "Alice's subs" from channel, user, user_subs
+where user_subs.channel_id = channel.id
+and user_subs.user_id = user.id
+and user.name = "Alice";
+
+
+select user.name as "name" from user, channel, user_subs
+where user_subs.channel_id = channel.id
+and channel.name = "#general"
+and user_subs.user_id = user.id;
+
+select message.content as "Alices messages", message.post_time as "Posted on" from message, user
+where message.user_id = user.id
+and user.name = "Alice";
+
+select message.content as "Bob's messages", message.post_time as "Posted on" from message, channel, user
+where message.user_id = user.id
+and message.channel_id = channel.id
+and user.name = "Bob"
+and channel.name = "#random";
+
+select count (message.content) as "Message Count", user.name as "User Name" from message, user
+where message.user_id = user.id
+group by user.id
+order by user.name desc
