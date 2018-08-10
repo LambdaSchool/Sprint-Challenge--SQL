a367ce3da874908fd550bb9ab87443d8b3438427
cgoez <goez2001@gmail.com>
2018-08-10 08:40:51 -0700

    Initial commit, create initial tables

---
+++ queries.sql
@@ -0,0 +1,26 @@
+CREATE TABLE organization (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL UNIQUE,
+)
+
+CREATE TABLE channel (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL UNIQUE,
+    organization_id INTEGER,
+    FOREIGN KEY(organization_id) REFERENCES organization(id)
+);
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL UNIQUE,
+)
+
+CREATE TABLE message (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    channel_id INTEGER,
+    content TEXT,
+    post_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES user(id),
+    FOREIGN KEY(channel_id) REFERENCES channel(id),
+)
