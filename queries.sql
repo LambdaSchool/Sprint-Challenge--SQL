e675c5252f84f786511fc0872215eff9bbf8afe4
Ashiko124 <greeneselectronics@gmail.com>
2018-08-10 10:58:25 -0600

    Tried to insert the data

--- queries.sql
+++ queries.sql
@@ -9,8 +9,8 @@ CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(128) NOT NULL,
     message_id INTEGER, --foreign key
-    user_id INTEGER, --foreign key
-    FOREIGN KEY(user_id) REFRENCES user(id),
+    channel_id INTEGER, --foreign key
+    FOREIGN KEY(user_id) REFRENCES channel(id),
     FOREIGN KEY(message_id) REFRENCES messages(id)
 );
 
@@ -30,3 +30,37 @@ CREATE TABLE channel (
     FOREIGN KEY(user_id) REFERENCES user(id)
 
 );
+
+
+
+
+INSERT INTO organization (name) VALUES ("Lambda School");
+
+
+INSERT INTO user (name) VALUES ("Alice");
+INSERT INTO user (name) VALUES ("Bob");
+INSERT INTO user (name) VALUES ("Chris");
+
+
+INSERT INTO channel (name) VALUES ("#general");
+INSERT INTO channel (name) VALUES ("#random");
+
+
+INSERT INTO channel (name, user_id) VALUES ('#general',1);
+INSERT INTO channel (name, user_id) VALUES ('#random',1);
+INSERT INTO channel (name, user_id) VALUES ('#general',2);
+INSERT INTO channel (name, user_id) VALUES ('#random',3);
+
+
+
+INSERT INTO user (message_id, name) VALUES ('hello bob', 2);
+INSERT INTO user (message_id, name) VALUES ('what a nice day', 1);
+INSERT INTO user (message_id, name) VALUES ('tommy is coming', 3);
+INSERT INTO channel (message_id, channel_id) VALUES ('once i knew the information', 3);
+INSERT INTO channel (message_id, channel_id) VALUES ('this is somewhat difficult', 4);
+
+INSERT INTO user (message_id, name) VALUES ('hello alice', 2);
+INSERT INTO user (message_id, name) VALUES ('what a nice day bob', 1);
+INSERT INTO user (message_id, name) VALUES ('tommy is coming home', 3);
+INSERT INTO channel (message_id, channel_id) VALUES ('once i knew the information i am king', 3);
+INSERT INTO channel (message_id, channel_id) VALUES ('this is somewhat difficult or so i assume', 4);
