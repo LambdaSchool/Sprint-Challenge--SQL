4bfb5d82dca1389ec8097e33b80cdbda4c28cfae
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-10 16:24:14 -0700

    "troubleshooted"

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+PRAGMA foreign_keys = ON;
 -- TABLES --
 
 CREATE TABLE organization (
@@ -42,12 +43,12 @@ INSERT INTO user (name) VALUES ('Alice');
 INSERT INTO user (name) VALUES ('Bob');
 INSERT INTO user (name) VALUES ('Chris');
 
-INSERT INTO channel (name, organization_id) VALUES ('#general', 1);
-INSERT INTO channel (name, organization_id) VALUES ('#random', 1);
+INSERT INTO channel (name, organization) VALUES ('#general', 1);
+INSERT INTO channel (name, organization) VALUES ('#random', 1);
 
 INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 1', 1, 1);
 INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 2', 3, 2);
-INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 3', 2, 3);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 3', 2, 2);
 INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 4', 3, 1);
 INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 5', 1, 1);
 INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 6', 1, 1);
@@ -61,21 +62,21 @@ INSERT INTO channel_joins (channel_id, user_id) VALUES (1, 2);
 
 INSERT INTO channel_joins (channel_id, user_id) VALUES (2, 1);
 
-INSERT INTO channel_joins (channel_id, user_id) VALUES (3, 2);
+INSERT INTO channel_joins (channel_id, user_id) VALUES (2, 3);
 
 -- SELECT--
 
-SELECT name FROM organization
+SELECT name FROM organization;
 
-SELECT name FROM channel
+SELECT name FROM channel;
 
 SELECT organization.name AS "organization", 
 channel.name AS "channel" FROM channel 
 join organization;
 
 SELECT message.content FROM message, 
-channel WHERE message.channel_id IS channel.id AND channel.name IS "#general" 
-ORDER  BY  post_time;
+channel WHERE message.channel IS channel.id AND channel.name IS "#general" 
+ORDER BY post_time;
 
 SELECT user.name AS "User Name", COUNT (message.content) AS "Message Count" FROM message, 
 user WHERE message.user = user.id GROUP BY user.id ORDER BY user.name DESC;
