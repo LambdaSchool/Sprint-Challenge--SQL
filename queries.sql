597596d060c17a6ac9b6f79ee7103778ccc76740
Harrison Brock <harrison@harrisonbrock.com>
2018-08-10 00:40:25 -0400

    Create user and organization tables in the create file this time

---
+++ queries.sql
@@ -0,0 +1,14 @@
+-- user table
+-- name must be unique
+create table user (
+  id Integer PRIMARY KEY AUTOINCREMENT,
+  name varchar(255) not null UNIQUE
+);
+
+-- organization table
+-- name must be unique
+create table organization (
+  id Integer PRIMARY KEY AUTOINCREMENT,
+  name varchar(255) not null UNIQUE
+
+);
