557503bc0363d1125770b8b48ff0a2144448d06a
Harrison Brock <harrison@harrisonbrock.com>
2018-08-10 12:17:05 -0400

    queries done

--- queries.sql
+++ queries.sql
@@ -51,7 +51,7 @@ insert into user(name) values ('Chris');
 
 -- insert into channels
 insert into channel (name, organization_fk) values ('#general', 1);
-insert into channel (name, organization_fk) values ('#random', 2);
+insert into channel (name, organization_fk) values ('#random', 1);
 
 -- Alice both channels
 insert into user_channel(channel_kf, user_fk) values (1,1);
@@ -84,4 +84,36 @@ insert into message(content, user_fk, channel_kf) values ('Alice random message 
 insert into message(content, user_fk, channel_kf) values ('Alice random message test 2', 2,2);
 insert into message(content, user_fk, channel_kf) values ('Alice random message test 3', 2,2);
 
+-- queries
+-- 1 list all organisation names
+select name from organization;
 
+-- 2 list all channel names
+select name from channel;
+
+-- 3
+SELECT ch.name from channel as ch, organization as org
+where ch.organization_fk = org.id and org.name = 'Lambda School';
+
+-- 4
+select msg.content from message as msg, channel as ch
+where msg.channel_kf = ch.id and
+ch.name = '#general';
+
+-- 5
+SELECT c.name AS Alice_Channels, c.id AS Channel_ID FROM channel AS c
+INNER JOIN user_channel AS cs ON c.id IS cs.channel_kf
+INNER JOIN user AS u ON u.id IS cs.user_fk WHERE u.name IS 'Alice';
+
+-- 6
+select u.name from user as u, channel as ch, user_channel as uc
+where uc.user_fk = u.id and ch.id = uc.channel_kf
+and ch.name = '#general';
+
+-- 7
+select mgs.content from message as mgs, user as u
+where mgs.user_fk = u.id and u.name = 'Alice';
+
+-- 8
+SELECT * FROM message WHERE id IS (SELECT id FROM channel WHERE name IS '#random')
+AND id IS (SELECT id FROM user WHERE name IS 'Bob');
