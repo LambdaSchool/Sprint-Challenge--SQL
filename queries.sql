ad4b613ca0d05991d68407b9e9bb8c4206831aab
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-10 10:53:03 -0700

    "inserts and selects"

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+-- TABLES --
+
 CREATE TABLE organization (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(128) NOT NULL UNIQUE,
@@ -25,5 +27,55 @@ CREATE TABLE message (
   channel INTEGER,
   FOREIGN KEY(user) REFERENCES user(id),
   FOREIGN KEY(channel) REFERENCES channel(id)
-)
+);
 
+CREATE TABLE channel_joins (
+  channel_id INTEGER REFERENCES channel(id),
+  user_id INTEGER REFERENCES user(id)
+);
+
+-- INSERT --
+
+INSERT INTO organization (name) VALUES ('Lambda School');
+
+INSERT INTO user (name) VALUES ('Alice');
+INSERT INTO user (name) VALUES ('Bob');
+INSERT INTO user (name) VALUES ('Chris');
+
+INSERT INTO channel (name, organization_id) VALUES ('#general', 1);
+INSERT INTO channel (name, organization_id) VALUES ('#random', 1);
+
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 1', 1, 1);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 2', 3, 2);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 3', 2, 3);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 4', 4, 1);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 5', 1, 1);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 6', 1, 1);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 7', 1, 1);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 8', 1, 1);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 9', 1, 1);
+INSERT INTO message (content, user, channel) VALUES('these are all the same...kinda 0', 1, 1);
+
+INSERT INTO channel_joins (channel_id, user_id) VALUES (1, 1);
+INSERT INTO channel_joins (channel_id, user_id) VALUES (1, 2);
+
+INSERT INTO channel_joins (channel_id, user_id) VALUES (2, 1);
+
+INSERT INTO channel_joins (channel_id, user_id) VALUES (3, 2);
+
+-- SELECT--
+
+SELECT name FROM organization
+
+SELECT name FROM channel
+
+SELECT organization.name AS "organization", 
+channel.name AS "channel" FROM channel 
+join organization;
+
+SELECT message.content FROM message, 
+channel WHERE message.channel_id IS channel.id AND channel.name IS "#general" 
+ORDER  BY  post_time;
+
+SELECT user.name AS "User Name", COUNT (message.content) AS "Message Count" FROM message, 
+user WHERE message.user = user.id GROUP BY user.id ORDER BY user.name DESC;
