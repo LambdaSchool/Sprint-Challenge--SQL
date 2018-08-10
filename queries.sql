c174be3b3e794543c6bf5a6f5e97a58123d7aedc
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-08-10 10:25:58 -0700

    Insert

--- queries.sql
+++ queries.sql
@@ -26,3 +26,28 @@ CREATE TABLE channel_user (
     channel_id INTEGER REFERENCES channel(id),
     user_id INTEGER REFERENCES user(id)
 );
+
+INSERT INTO organization (name) VALUES ('Lambda School');
+
+INSERT INTO user (name) VALUES ('Alice');
+INSERT INTO user (name) VALUES ('Bob');
+INSERT INTO user (name) VALUES ('Chris');
+
+INSERT INTO channel (name, organization_id) VALUES ('#general', 1);
+INSERT INTO channel (name, organization_id) VALUES ('#random', 1);
+
+INSERT INTO channel_user (channel_id, user_id) VALUES (1, 1);
+INSERT INTO channel_user (channel_id, user_id) VALUES (2, 1);
+INSERT INTO channel_user (channel_id, user_id) VALUES (1, 2);
+INSERT INTO channel_user (channel_id, user_id) VALUES (2, 3);
+
+INSERT INTO message (content, user_id, channel_id) VALUES ('Hi', 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Sup', 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Ohai', 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Yahello', 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Howdy do?', 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Heyo', 2, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Greetings', 3, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('See ya', 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Salutations', 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Ciao', 3, 2);
