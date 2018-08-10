4e509e3476c8651f32c4aa07cc2dfda329836b38
sadrak105 <sadrak10@gmail.com>
2018-08-10 13:25:31 -0400

    channel table

--- queries.sql
+++ queries.sql
@@ -1,10 +1,21 @@
-
+PRAGMA foreign_keys = ON;
 
 
 -- Organization Table
 
-CREATE TABLE organization = (
+CREATE TABLE organization (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(128) NOT NULL UNIQUE,
+  name VARCHAR(128) NOT NULL,
   channel INTEGER REFRENCES channel(id)
 );
+
+-- Channel Table
+
+CREATE TABLE channel (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL,
+    organization_id INTEGER NOT NULL,
+    FOREIGN KEY(organization) REFERERENCES organization(id)
+   );
+
+
