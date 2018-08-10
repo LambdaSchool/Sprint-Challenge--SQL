4a6fe049fe9d43a82792efaa40a38da4eefee2c1
20djm <mostdarryl@gmail.com>
2018-08-10 13:07:00 -0400

    added foreign key to messages

--- queries.sql
+++ queries.sql
@@ -6,7 +6,7 @@ CREATE TABLE channel(id integer primary key autoincrement, name text, organizati
 
 CREATE TABLE user_channel(id integer primary key autoincrement, userId integer, channelId integer, foreign key(userId) references user(id), foreign key(channelId) references channel(id));
 
-CREATE TABLE message(id integer primary key autoincrement, content text, userId integer, post_time text, foreign key(userId) references user(id));
+CREATE TABLE message(id integer primary key autoincrement, content text, userId integer, channelId integer, post_time text, foreign key(userId) references user(id), foreign key(channelId) references channel(id));
 
 insert into organization(name) values("Lambda School");
 
