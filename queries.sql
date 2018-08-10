637dec485a60b71d1568e7a4b99ffa94f246475e
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-08-10 11:31:46 -0400

    added inserts

--- queries.sql
+++ queries.sql
@@ -9,16 +9,22 @@ CREATE TABLE organization = (
 CREATE TABLE channel = (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(128) NOT NULL UNIQUE,
-  users INTEGER REFRENCES user(id)
+  users INTEGER REFRENCES user(id),
+  organization INTEGER REFRENCES organization(id)
 );
 
 -- user table
 CREATE TABLE user = (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(128) NOT NULL UNIQUE,
-  channels INTEGER REFRENCES channel(id),
   messages INTEGER REFRENCES message(id)
 );
+
+-- user channel table 
+CREATE TABLE user_channel = (
+  user INTEGER REFRENCES user(id),
+  channel INTEGER REFRENCES channel(id),
+)
 
 -- message table
 CREATE TABLE message = (
@@ -29,3 +35,24 @@ CREATE TABLE message = (
   channel INTEGER REFRENCES channel(id)
 );
 
+-- insert organization
+insert into organization (name) values ('Lambda School');
+
+-- insert 3 users 
+insert into user(name) values ('Alice');
+insert into user(name) values ('Bob');
+insert into user(name) values ('Chris');
+
+-- insert 2 channels
+insert into channel (name, organization) values ('#general', 1);
+insert into channel (name, organization) values ('#random', 2);
+
+-- insert Alice 
+insert into user_channel (channel, user) values (1,1);
+insert into user_channel (channel, user) values (2,1);
+
+-- insert Bob 
+insert into user_channel (channel, user) values (1,2);
+
+-- insert Chris
+insert into user_channel (channel, user) values (2,2);
