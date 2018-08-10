99911d756ed740f572ba2fdcef49b1e6199d93a5
Henry K <mykim1891@hotmail.com>
2018-08-10 11:20:29 -0700

    MVP completed along with stretch

--- queries.sql
+++ queries.sql
@@ -10,7 +10,7 @@ CREATE TABLE channel (
   FOREIGN KEY(org_id) REFERENCES organization(id)
 );
 
-CREATE TABLE users (
+CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,    
   name VARCHAR(128) NOT NULL
 );
@@ -33,19 +33,11 @@ CREATE TABLE message (
   FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
--- CREATE TABLE user_messages (
---   id INTEGER PRIMARY KEY AUTOINCREMENT,
---   user_id INTEGER,
---   channel_id INTEGER
---   FOREIGN KEY user_id REFERENCES user(id)
---   FOREIGN KEY message_id REFERENCES channel(id)
--- )
-
 INSERT INTO organization (name) VALUES ('Lambda School');
 
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Bob');
-INSERT INTO users (name) VALUES ('Chris');
+INSERT INTO user (name) VALUES ('Alice');
+INSERT INTO user (name) VALUES ('Bob');
+INSERT INTO user (name) VALUES ('Chris');
 
 INSERT INTO channel (name, org_id) VALUES ('#general', 1);
 INSERT INTO channel (name, org_id) VALUES ('#random', 1);
@@ -66,3 +58,23 @@ INSERT INTO user_channel (user_id, channel_id) VALUES (1, 1);
 INSERT INTO user_channel (user_id, channel_id) VALUES (1, 2);
 INSERT INTO user_channel (user_id, channel_id) VALUES (2, 1);
 INSERT INTO user_channel (user_id, channel_id) VALUES (3, 2);
+
+-- SELECT Task 1-5
+SELECT name from organization;
+SELECT name from channel;
+SELECT channel.name from channel, organization where channel.org_id = organization.id;
+SELECT content from message ORDER BY post_time DESC;
+SELECT channel.name from channel, user, user_channel where user.id = user_channel.user_id AND channel.id = user_channel.channel_id and user.name = 'Alice';
+-- SELECT Task 6-9
+SELECT user.name from channel, user, user_channel where user.id = user_channel.user_id AND channel.id = user_channel.channel_id and channel.name = '#general';
+SELECT message.content from user, message where message.user_id = user.id and user.name = 'Alice';
+SELECT message.content from user, message, channel where message.user_id = user.id and message.channel_id = channel.id and user.name = 'Bob' and channel.name = '#random';
+SELECT user.name as User_Name, count(message.content) as Messagee_Count from user, message where message.user_id = user.id group by user.name order by user.name DESC;
+-- Stretch
+SELECT user.name as User_Name, channel.name as Channel, count(message.content) as Messagee_Count 
+from channel, user, message 
+where message.user_id = user.id, message.channel_id = channel.id
+group by user.name, channel.name 
+order by user.name DESC;
+
+-- For Question 6: using ON DELETE CASCADE will delete child data from child table as soon as the parent data is deleted.
