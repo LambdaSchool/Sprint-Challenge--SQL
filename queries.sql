e0922c078878532a32216c82a582314d4987f48e
Ashiko124 <greeneselectronics@gmail.com>
2018-08-14 13:58:17 -0600

    Got everything done but having some weird errors

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
+.mode column
+.header on
+-- Create a DB for the Online Chat System
+
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE organization (
@@ -8,26 +12,27 @@ CREATE TABLE organization (
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(128) NOT NULL,
-    date TIMESTAMP
+    posted_at DATETIME
     DEFAULT CURRENT_TIMESTAMP
 );
 
+
 CREATE TABLE messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    post_time TEXT TIMESTAMP
-    DEFAULT CURRENT_TIMESTAMP,
     content VARCHAR(2000) NOT NULL,
-    channel_id INTEGER,
-    FOREIGN KEY(channel_id) REFERENCES channel(id), 
+    channel_id INTEGER, 
     user_id INTEGER,
-    FOREIGN KEY(user_id) REFERENCES user(id), 
+    post_time TIMESTAMP
+    DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(channel_id) REFERENCES channel(id),
+    FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
 CREATE TABLE channel (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(128) NOT NULL,
     organization_id INTEGER, --foreign key
-    FOREIGN KEY(organization_id) REFERENCES organization(id),
+    FOREIGN KEY(organization_id) REFERENCES organization(id)
 
 );
 
@@ -38,65 +43,78 @@ CREATE TABLE channel (
 
 CREATE TABLE channel_subs (
     channel_id INTEGER,
-    FOREIGN KEY(channel_id) REFERENCES channel(id),
-    user_id INTEGER,
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    users_id INTEGER,
+    FOREIGN KEY(users_id) REFERENCES user(id),
+    FOREIGN KEY(channel_id) REFERENCES channel(id)
 );
-)
-
-
-INSERT INTO channel.user_id (message_id, name) VALUES ('hello bob', 'bob');
 
 
 
 
 
-INSERT INTO organization (name) VALUES ("Lambda School");
+-- insert one organization -- 
+ INSERT INTO organization (name) VALUES ("Lambda School");
 
+-- insert 3 users -- 
+ INSERT INTO user (name) VALUES ("Alice");
+ INSERT INTO user (name) VALUES ("Bob");
+ INSERT INTO user (name) VALUES ("Chris");
 
-INSERT INTO user (name) VALUES ("Alice");
-INSERT INTO user (name) VALUES ("Bob");
-INSERT INTO user (name) VALUES ("Chris");
+ -- insert two channels --
+ INSERT INTO channel (name, organization_id) VALUES ("#general", 1);
+ INSERT INTO channel (name, organization_id) VALUES ("#random", 1);
 
-
-INSERT INTO channel (name) VALUES ("#general");
-INSERT INTO channel (name) VALUES ("#random");
-
-
-INSERT INTO channel (name, user_id) VALUES ('#general',1);
-INSERT INTO channel (name, user_id) VALUES ('#random',1);
-INSERT INTO channel (name, user_id) VALUES ('#general',2);
-INSERT INTO channel (name, user_id) VALUES ('#random',3);
-
-
-
-INSERT INTO user (message_id, name) VALUES ('hello bob', 'bob');
--- INSERT INTO user (message_id, name) VALUES ('what a nice day', 'Alice');
--- INSERT INTO user (message_id, name) VALUES ('tommy is coming', 'Chris');
--- INSERT INTO channel (message_id, channel_id) VALUES ('once i knew the information', 3);
--- INSERT INTO channel (message_id, channel_id) VALUES ('this is somewhat difficult', 4);
-
--- INSERT INTO user (message_id, name) VALUES ('hello alice', 2);
--- INSERT INTO user (message_id, name) VALUES ('what a nice day bob', 1);
--- INSERT INTO user (message_id, name) VALUES ('tommy is coming home', 3);
--- INSERT INTO channel (message_id, channel_id) VALUES ('once i knew the information i am king', 3);
--- INSERT INTO channel (message_id, channel_id) VALUES ('this is somewhat difficult or so i assume', 4);
+-- insert certain users into certain channels --
+-- Alice should be in #general and #random. --
+-- Bob should be in #general. --
+-- Chris should be in #random. --
+ INSERT INTO channel_subs (channel_id, users_id) VALUES (1,1);
+ INSERT INTO channel_subs (channel_id, users_id) VALUES (2,1);
+ INSERT INTO channel_subs (channel_id, users_id) VALUES (1,2);
+ INSERT INTO channel_subs (channel_id, users_id) VALUES (2,2);
 
 
 
--- SELECT * FROM organization;
+INSERT INTO messages (content, channel_id, user_id) VALUES ('hello bob this is a message', 1, 1);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('hello alice this is a message', 1, 2);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('hello jim this is a message', 1, 3);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('hello tammy this is a message', 2, 1);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('hello carie this is a message', 2, 1);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('hello bob this is a test', 2, 1);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('The force is strong with this one', 1, 2);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('I wonder what my limit is? ', 2, 3);
+INSERT INTO messages (content, channel_id, user_id) VALUES ("What happens if i quote inside this statment", 1, 2);
+INSERT INTO messages (content, channel_id, user_id) VALUES ('hello bob this is a message2', 1, 2);
+
+-- List all organization names --
+SELECT * FROM organization;
+
+-- List all channel names. --
+SELECT * FROM channel;
+
+-- List all channels in a specific organization by organization name. --
+SELECT * FROM channel.name where organization.name = 'Lambda School';
 
 
--- SELECT * FROM channel;
+-- List all messages in a specific channel by channel name #general in order of post_time, descending. (Hint: ORDER BY. --
+-- Because your INSERTs might have all taken place at the exact same time, this might not return meaningful results. --
+-- But humor us with the ORDER BY anyway.) --
+SELECT * FROM messages where channel_id = 1 ORDER BY post_time desc;
 
--- SELECT * FROM channel.name where organization.name = 'Lambda School';
+-- List all channels to which user Alice belongs. --
+SELECT * FROM channel, channel_subs, user where channel_subs.users_id = user.id AND user.name = 'Alice';
 
+-- List all users that belong to channel #general. --
+SELECT * FROM user, channel_subs, channel where channel_subs.channel_id = channel.id AND channel.name = '#general';
 
--- SELECT * FROM channel, user where channel.user_id = user.id AND user.name = 'Alice';
+-- List all messages in all channels by user Alice --
+SELECT * FROM messages, channel_subs, user where channel_subs.users_id = user.id AND user.name = 'Alice';
 
--- SELECT * FROM user.name, channel where user.channel_id = channel.id AND channel.name = '#general';
+-- List all messages in #random by user Bob. --
+SELECT * FROM messages, user, channel_subs, channel where channel_subs.channel_id = channel.id AND channel.name = '#random' AND user.name = 'Bob';
 
--- SELECT * from messages, user where user.name = 'Alice';
-
+-- List the count of messages across all channels per user. (Hint: COUNT, GROUP BY.) --
+-- The title of the user's name column should be User Name and the title of the count column should be Message Count. --
+SELECT user.name, count(*) FROM user, messages GROUP BY messages.user_id;
 -- SELECT * from messages, channel, user where channel.name = '#random' AND channel.user_id = user.id AND user.name = 'bob';
 
