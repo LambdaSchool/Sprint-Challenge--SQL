4ee0824dfb42ff685f892fbfb217cfed4cf5da63
Bee C <b.choeung@gmail.com>
2018-09-07 13:01:31 -0400

    created all tables and inserted info for organization, usre, channel and now working inserting info into messages

--- queries.sql
+++ queries.sql
@@ -1,1 +1,48 @@
--- making comment to make pull request--
+--What SQL keywords or concept would you use if you wanted to automatically delete all messages by a user if that user were deleted from the user table?--
+
+PRAGMA foreign_keys = ON;
+
+-- organization table --
+CREATE TABLE organization (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(64) NOT NULL UNIQUE
+);
+
+-- channel table --
+CREATE TABLE channel (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(64) NOT NULL UNIQUE
+);
+
+-- user table --
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(64) NOT NULL UNIQUE
+);
+
+-- message table --
+CREATE TABLE messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user_id INTEGER REFERENCES user(id),
+    channel_id INTEGER REFERENCES channel(id),
+    post_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- insert into organization --
+INSERT INTO organization (name) VALUES ("Lambda School");
+
+-- insert into user --
+INSERT INTO user (name) VALUES ("Alice");
+INSERT INTO user (name) VALUES ("Bob");
+INSERT INTO user (name) VALUES ("Chris");
+
+-- insert into channel --
+INSERT INTO channel (name) VALUES ("#general");
+INSERT INTO channel (name) VALUES ("#random");
+
+-- insert into messages --
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Fly me to the moon!", 1, 1);
+
+
