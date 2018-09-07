173af8a2a6cb5a6bcd0d5e5791efb48de289e59f
Bee C <b.choeung@gmail.com>
2018-09-07 13:23:52 -0400

    completed all INSERT

--- queries.sql
+++ queries.sql
@@ -44,5 +44,13 @@ INSERT INTO channel (name) VALUES ("#random");
 
 -- insert into messages --
 INSERT INTO messages (content, user_id, channel_id) VALUES ("Fly me to the moon!", 1, 1);
-
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Love for all, hatred for none.", 1, 2);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Change the world by being yourself.", 2, 1);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Every moment is a fresh beginning.", 2, 1);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Never regret anything that made you smile.", 3, 2);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Everything you can imagine is real.", 3, 2);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Simplicity is the ultimate sophistication.", 1, 1);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Reality is wrong, dreams are for real.", 1, 2);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("And still I rise.", 2, 1);
+INSERT INTO messages (content, user_id, channel_id) VALUES ("Turn your wounds into wisdom.", 3, 2);
 
