38d13b7b9d8a90204fb911783d67afd18ab0bb1b
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-09-07 12:10:17 -0400

    Insert data in organization, user and channel tables

--- queries.sql
+++ queries.sql
@@ -38,3 +38,17 @@ CREATE TABLE user_channel
   user_id INTEGER REFERENCES user(id),
   channel_id INTEGER REFERENCES channel(id)
 );
+
+
+--Insert data into databse
+INSERT INTO organization (name) VALUES("Lambda School");
+
+INSERT INTO user (name, username) VALUES("Alice", "littlegirl");
+
+INSERT INTO user (name, username) VALUES("Bob", "theboss");
+
+INSERT INTO user (name, username) VALUES("Chris", "coolguy");
+
+INSERT INTO channel (name, organization_id) VALUES("#general", 1);
+
+INSERT INTO channel (name, organization_id) values("#random", 1);
