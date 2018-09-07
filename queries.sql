a877dae241487d5f41bc00588f8b869bc3a206d3
udykas <udykas@gmail.com>
2018-09-07 11:41:59 -0400

    First tables created, need to create m:m relationship tables next

--- queries.sql
+++ queries.sql
@@ -7,3 +7,31 @@ PRAGMA foreign_keys = ON
 /*Turn on column out put and headers for `SELECT`*/
 .mode COLUMN
 .header ON
+
+
+-- CREATE TABLES
+CREATE TABLE organization (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL
+
+);
+
+CREATE TABLE channel (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL,
+    organization_id INT REFERENCES organization(id)
+);
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL,
+    organization_id INT REFERENCES organization(id)
+);
+
+CREATE TABLE message (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    content VARCHAR(300) NOT NULL,
+    channel_id INT REFERENCES channel(id),
+    user_id INT REFERENCES user(id)
+);
