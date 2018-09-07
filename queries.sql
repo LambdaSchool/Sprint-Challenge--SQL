2589b8ab827a2b97f5edbc9e86eb555dea070c5a
Zach Campbell <beazlebubbles@gmail.com>
2018-09-07 12:13:43 -0600

    Added messages

--- queries.sql
+++ queries.sql
@@ -86,3 +86,27 @@ INSERT INTO channel
 INSERT INTO channel
   (name, organization_id)
   VALUES("#livingwithcorprus", 2);
+
+INSERT INTO message
+  (content, user_id, channel_id)
+  VALUES("The sage who suppresses his best aphorism: cut off his hands, for he is a thief.", 1, 1);
+
+INSERT INTO message
+  (content, user_id, channel_id)
+  VALUES("lol wut?", 2, 1);
+
+INSERT INTO message
+  (content, user_id, channel_id)
+  VALUES("What do you guys do when your followers pray to you too much?", 3, 2);
+
+INSERT INTO message
+  (content, user_id, channel_id)
+  VALUES("There is once more the case of the symbolic and barren. The true prince that is cursed and demonized will be adored at last with full hearts. According to the Codes of Mephala there can be no official art, only fixation points of complexity that will erase from the awe of the people given enough time. This is a secret that hides another. An impersonal survival is not the way of the ruling king. Embrace the art of the people and marry it and by that I mean secretly have it murdered.", 1, 2);
+
+INSERT INTO message
+  (content, user_id, channel_id)
+  VALUES("Uh, OK, thanks.", 3, 2);
+
+INSERT INTO message
+  (content, user_id, channel_id)
+  VALUES("Guys, I got locked out of the ALMSIVI channels, you need to reinvite me.  Guys?  Are you seeing this?", 4, 3);
