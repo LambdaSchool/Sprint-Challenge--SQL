cc8a239b899b4c5664ece50c839946db4b8bd0e8
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-09-07 09:53:22 -0700

    auto increment ids

--- queries.sql
+++ queries.sql
@@ -1,12 +1,12 @@
 -- CREATE TABLES
 CREATE TABLE organization {
-  id int NOT NULL,
+  id int AUTOINCREMENT,
   organization_name varchar(255) NOT NULL,
   PRIMARY KEY (id)
 };
 
 CREATE TABLE channel {
-  id int NOT NULL,
+  id int AUTOINCREMENT,
   channel_name varchar(255) NOT NULL,
   organization_id int NOT NULL,
   PRIMARY KEY (id),
@@ -14,13 +14,13 @@ CREATE TABLE channel {
 };
 
 CREATE TABLE user {
-  id int NOT NULL,
+  id int AUTOINCREMENT,
   person_name varchar(255) NOT NULL,
   PRIMARY KEY (id),
 };
 
 CREATE TABLE message {
-  id int NOT NULL,
+  id int AUTOINCREMENT,
   message_content varchar(255) NOT NULL,
   channel_id int NOT NULL,
   user_id int NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE message {
 };
 
 CREATE TABLE subscriptions {
-  id int NOT NULL,
+  id int AUTOINCREMENT,
   channel_id int NOT NULL,
   user_id int NOT NULL,
   PRIMARY KEY (id),
@@ -40,31 +40,31 @@ CREATE TABLE subscriptions {
 
 -- INSERT DATA
 INSERT INTO organization
-VALUES (1, 'Lambda School');
+VALUES ('Lambda School');
 
 INSERT INTO user
-VALUES (1, 'Alice');
+VALUES ('Alice');
 
 INSERT INTO user
-VALUES (2, 'Bob');
+VALUES ('Bob');
 
 INSERT INTO user
-VALUES (3, 'Chris');
+VALUES ('Chris');
 
 INSERT INTO channel
-VALUES (1, '#general', 1);
+VALUES ('#general', 1);
 
 INSERT INTO channel
-VALUES (2, '#random', 1);
+VALUES ('#random', 1);
 
 INSERT INTO subscriptions
-VALUES (1, 1, 1);
+VALUES (1, 1);
 
 INSERT INTO subscriptions
-VALUES (2, 2, 1);
+VALUES (2, 1);
 
 INSERT INTO subscriptions
-VALUES (3, 1, 2);
+VALUES (1, 2);
 
 INSERT INTO subscriptions
-VALUES (4, 2, 3);
+VALUES (2, 3);
