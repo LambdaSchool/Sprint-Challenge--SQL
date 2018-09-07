bda325e6c053e36e5ceb5cccad9e44fdea6fee8d
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-09-07 13:25:19 -0400

    Inserted queries to add information to the database.

--- queries.sql
+++ queries.sql
@@ -24,7 +24,7 @@ END;
     -- user. This table should at least have column(s): 
     -- name    
 
-CREATE TABLE user (id PRIMARY KEY AUTOINCREMENT, username VARCHAR(100) NOT NULL UNIQUE, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+CREATE TABLE user (id PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(100) NOT NULL, last_name VARCHAR(100) NOT NULL, username VARCHAR(50) NOT NULL UNIQUE, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
 
 CREATE TRIGGER UPDATE_TIMESTAMP_TRIGGER_FOR_USER
 AFTER UPDATE ON user 
@@ -34,9 +34,9 @@ END;
 
     -- message. This table should have at least columns(s):
     -- post_time--the timestamp of when the message was posted
-    -- content--the message content itself
+      content--the messa  content itself
 
-CREATE TABLE message (id PRIMARY KEY AUTOINCREMENT, content TEXT, user_id INTEGER REFERENCES user(id) ON DELETE CASCADE, channel_id INTEGER REFERENCES channel(id) ON DELETE CASCADE, post_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+CREATE TABLE message (id PRIMARY KEY AUTOINCREMEN  content TEXT, user_id INTEGER REFERENCES user(id) ON DELETE CASCADE, channel_id INTEGER REFERENCES channel(id) ON DELETE CASCADE, post_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
 
 CREATE TRIGGER UPDATE_TIMESTAMP_TRIGGER_FOR_MESSAGE
 AFTER UPDATE ON message
@@ -48,6 +48,40 @@ END;
 -- 3. Add additional join tables needed, if any.
 
 CREATE TABLE users_channel (users_id INTEGER REFERENCES user(id) ON DELETE CASCADE, channel_id INTEGER REFERENCES channel(id) ON DELETE CASCADE) 
+
+-- 4. Write INSERT queries to add information to the database.
+
+    -- One organization, Lambda School
+
+INSERT INTO organization (name) VALUES ('Lambda School')
+
+    -- Three users, Alice, Bob, and Chris
+
+INSERT INTO user (username) VALUES ('Alice', 'Wonderland', 'alice20')
+INSERT INTO user (username) VALUES ('Bob', 'Dillan', 'bobby76')
+INSERT INTO user (username) VALUES ('Chris', 'Hay', 'chrisp9')
+
+    -- Two channels, #general and #random
+
+INSERT INTO channel (name, organization_id) VALUES ('#general', 1)
+INSERT INTO channel (name, organization_id) VALUES ('#random', 1) 
+
+    -- 10 messages (at least one per user, and at least one per channel).
+    -- Alice should be in #general and #random.
+    -- Bob should be in #general.
+    -- Chris should be in #random.
+
+INSERT INTO message (content, user_id, channel_id) VALUES("I'M THE KING OF THE WORLD", 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES("OH, you did an actual query.  That’s awesome lol", 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES("Cats are cool.", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES("So many repos.", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES("Don't worry guys.", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES("Why do I type so slow?", 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES("Two hours of sleep is rough.", 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES("What's the repo for today?", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES("Can you make a PR request please", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES("Do you think I will graduate?", 1, 2);
+
 
 
 
