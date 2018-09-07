e6c301ffbed89e77137ef2f8a6df59a96efee78d
derrick-mei <derrick.mei.168@gmail.com>
2018-09-07 02:39:28 -0500

    insert data

--- queries.sql
+++ queries.sql
@@ -47,3 +47,36 @@ CREATE TABLE user_channel (
     channel_id INTEGER REFERENCES channel(id)
 );
 
+/**/
+INSERT INTO organization (name) VALUES("Lambda School");
+
+/**/
+INSERT INTO user (name) VALUES ("Alice");
+INSERT INTO user (name) VALUES ("Bob");
+INSERT INTO user (name) VALUES ("Chris");
+
+/**/
+INSERT INTO channel (name, organization_id) VALUES ("#general", 1);
+INSERT INTO channel (name, organization_id) VALUES ("#random", 1);
+
+/**/
+INSERT INTO message (content, user_id, channel_id) VALUES ("1 DAMN DANIEL", 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("2 I'M THE KING OF THE WORLD", 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("3 LAMBDA4LIFE", 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("4 I'm so exited for labs!!!", 2, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("5 I can't wait to be a grown up", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("6 Cat all over me", 2, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("7 Javascript is my favorite", 3, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("8 Life is 42", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("9 what is the purpose of life", 3, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("10 I'm finished!!!", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("11 I'm finished for real!!!", 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("12 I'm finished for real for real!!!", 1, 1);
+
+/**/
+INSERT INTO user_channel (user_id, channel_id) VALUES (1, 1);
+INSERT INTO user_channel (user_id, channel_id) VALUES (1, 2);
+INSERT INTO user_channel (user_id, channel_id) VALUES (2, 1);
+INSERT INTO user_channel (user_id, channel_id) VALUES (3, 2);
+
+
