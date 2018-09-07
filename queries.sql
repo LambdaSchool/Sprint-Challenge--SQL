f7a911feac4617cabb115fc1dd017c5f098c9e18
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-09-08 00:04:30 +0800

    Wrote INSERT queries to populate tables/databases.

--- queries.sql
+++ queries.sql
@@ -33,3 +33,30 @@ CREATE TABLE message (
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Writing the INSERT queries to populate the tables/databases.
+
+-- One organization, Lambda School
+INSERT INTO organization (name, username) VALUES ("Lambda School");
+
+-- Three users, Alice, Bob, and Chris
+INSERT INTO user (name, username) VALUES ("Alice", "aliceinchains");
+INSERT INTO user (name, username) VALUES ("Bob", "bobmarley");
+INSERT INTO user (name, username) VALUES ("Chris", "chriscornell");
+
+-- Two channels, #general and #random
+INSERT INTO channel (name, organization_id) VALUES ("#general", 1);
+INSERT INTO channel (name, organization_id) VALUES ("#random", 2);
+
+-- 10 messages (at least one per user, and at least one per channel).
+INSERT INTO message (content, user_id, channel_id) VALUES ("Hello, I'm Alice.", 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Hey, I'm Bob.", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Hi, I'm Chris.", 3, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("You know, from Alice in Wonderland.", 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Marley.", 2, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Just Chris.", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("I'm serious. I'm not a figment of your imagination.", 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("But you can call me Bob Marley and the Wailers, for short.", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Family name's also Chris. So call me Chris Chris.", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Bye!", 1, 1);
+
