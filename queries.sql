dad8e961f16b2a94b97fd1413f9ddf97036186d4
udykas <udykas@gmail.com>
2018-09-07 12:27:17 -0400

    Added INSERTs

--- queries.sql
+++ queries.sql
@@ -8,7 +8,6 @@ PRAGMA foreign_keys = ON
 .mode COLUMN
 .header ON
 
-
 -- CREATE TABLES
 CREATE TABLE organization (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,12 +37,40 @@ CREATE TABLE message (
 
 -- JOIN TABLES
 
-CREATE TABLE organization_user (
-    organization_id INT REFERENCES organization(id),
-    user_id INT REFERENCES user(id)
-);
+-- CREATE TABLE organization_user (
+--     organization_id INT REFERENCES organization(id),
+--     user_id INT REFERENCES user(id)
+-- );
 
 CREATE TABLE channel_user (
     user_id INT REFERENCES user(id),
     channel_id INT REFERENCES channel(id)
 );
+
+-- INSERT
+
+INSERT INTO organization (name) VALUES ("Lambda School");
+
+INSERT INTO user (name, organization_id) VALUES ("Alice", 1);
+INSERT INTO user (name, organization_id) VALUES ("Bob", 1);
+INSERT INTO user (name, organization_id) VALUES ("Chris", 1);
+
+INSERT INTO channel (name, organization_id) VALUES ("#general", 1);
+INSERT INTO channel (name, organization_id) VALUES ("#random", 1);
+
+INSERT INTO message (content, user_id, channel_id) VALUES ("Chris is so random", 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Bob is so basic", 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("CHECK THIS CRAZY VIDEO OUT", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Ha, yeah", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Somebody at google was just like 'yea, just have someone drive down every road on f@#%ing earth.' And you know what? They did it.", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("Anxiety is like when video game combat music is playing but you can't find any enemies.", 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("I like bread", 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ("What's your favorite language? Ruby or Javascript?", 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("I prefer ArnoldC :P" , 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ("...Can't you take anything seriously?!", 1, 2);
+
+INSERT INTO channel_user (user_id, channel_id) VALUES (1, 1);
+INSERT INTO channel_user (user_id, channel_id) VALUES (1, 2);
+INSERT INTO channel_user (user_id, channel_id) VALUES (2, 1);
+INSERT INTO channel_user (user_id, channel_id) VALUES (3, 2);
+
