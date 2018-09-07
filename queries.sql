dde1f3d190d3ab5db4ccea3cdd25005ad656d402
David <AquilaVirtual>
2018-09-07 15:35:51 -0700

    refactored for cleaner code: changed SELECT queries

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ CREATE TABLE organization (
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(128) NOT NULL
+    name VARCHAR(128),
     organization_id INTEGER REFERENCES organization(id)
 
 );
@@ -27,8 +27,8 @@ CREATE TABLE channel_user (
 CREATE TABLE message (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     content VARCHAR(2056) NOT NULL,
-    timestamp DATE DEFAULT (datetime('now', 'localtime'))
-    channel_id INTEGER REFERENCES channel(id)
+    timestamp DATE DEFAULT (datetime('now', 'localtime')),
+    channel_id INTEGER REFERENCES channel(id),
     user_id INTEGER REFERENCES user(id)
 );
 
@@ -50,13 +50,14 @@ INSERT INTO channel_user (channel_id, user_id) VALUES (2,3);
 INSERT INTO message (content, channel_id,  user_id) VALUES ("Alice's post in #general", 1, 1);
 INSERT INTO message (content, channel_id,  user_id) VALUES ("Alice's post in #random", 2, 1);
 INSERT INTO message (content, channel_id,  user_id) VALUES ("Alice's post in #general again", 1, 1);
-INSERT INTO message (content, channel_id,  user_id) VALUES ("Bob's post in #random", 2, 2);
-INSERT INTO message (content, channel_id,  user_id) VALUES ("Bob's post in #general", 1, 2);
-INSERT INTO message (content, channel_id,  user_id) VALUES ("Bob's post in #general again", 1, 2);
-INSERT INTO message (content, channel_id;  user_id) VALUES ("Alice's second post in #random again", 2, 1);
+INSERT INTO message (content, channel_id,  user_id) VALUES ("Bob's first post in #random", 2, 2);
+INSERT INTO message (content, channel_id,  user_id) VALUES ("Bob's  second post  in #general", 1, 2);
+INSERT INTO message (content, channel_id,  user_id) VALUES ("Bob's third post in #general again", 1, 2);
 INSERT INTO message (content, channel_id,  user_id) VALUES ("Chris' first post in #random", 2, 3);
 INSERT INTO message (content, channel_id,  user_id) VALUES ("Chris' post in #general", 1, 3);
 INSERT INTO message (content, channel_id,  user_id) VALUES ("Chris' second post in #random", 2, 3);
+INSERT INTO message (content, channel_id,  user_id) VALUES ("Another by Alice in #random", 2, 1);
+
 
 -- List all organization names
 SELECT * FROM organization;
@@ -65,32 +66,26 @@ SELECT * FROM organization;
 SELECT * FROM channel;
 
 -- List all channels in a specific organization by name
-SELECT channel.name, organization.name 
-FROM organization, channel 
-WHERE channel.organization_id = organization.id 
+SELECT channel.name FROM channel, organization
+WHERE organization_id = organization.id
 AND organization.name = "Lambda School";
 
 -- List all messages in a specific channel by channel name #general in order of timestamp, descending
-SELECT message.content, message.timestamp, channel.name 
-FROM message, channel, user, channel_user 
-WHERE message.user_id = channel_user.id 
-AND channel_user.channel_id = channel.id 
-AND channel_user.user_id = user.id 
+SELECT content FROM message, channel
+WHERE channel_id = channel.id 
 AND channel.name = "#general" 
 ORDER BY timestamp DESC;
 
 -- List all channels to which user Alice belongs
-SELECT channel.* 
-FROM channel, user, channel_user 
-WHERE channel_user.channel_id = channel.id 
-AND channel_user.user_id = user.id 
+SELECT channel.name FROM channel, user, channel_user  
+WHERE user.id = user_id
+AND channel.id = channel_id
 AND user.name = "Alice";
 
 -- List all users that belong to channel #general
-SELECT user.* 
-FROM channel, user, channel_user 
-WHERE channel_user.channel_id = channel.id 
-AND channel_user.user_id = user.id 
+SELECT user.name FROM channel, user, channel_user 
+WHERE user_id = user.id
+AND channel.id = channel_id
 AND channel.name = "#general";
 
 -- List all messages in all channels by user Alice
@@ -101,25 +96,25 @@ AND channel_user.channel_id = channel.id
 AND channel_user.user_id = user.id 
 AND user.name = "Alice";
 
+SELECT content FROM message, user
+WHERE user_id = user.id
+AND user.name = 'Alice';
+
 -- List all messages in #random by user Bob
-SELECT message.*, channel.name 
-FROM message, channel, user, channel_user 
-WHERE message.channel_user_id = channel_user.id 
-AND channel_user.channel_id = channel.id 
-AND channel_user.user_id = user.id 
-AND user.name = "Bob" 
-AND channel.name= "#random";
+SELECT content FROM Message, channel, user
+WHERE user_id = user.id
+AND channel_id = channel.id
+AND channel.name = "#random"
+AND User.name = "Bob";
 
 -- List the count of messages across all channels per user
-SELECT user.name AS "User Name", COUNT(*) AS "Message Count" 
-FROM message, channel, user, channel_user 
-WHERE message.user_id = channel_user.id 
-AND channel_user.channel_id = channel.id 
-AND channel_user.user_id = user.id 
-GROUP BY user_id 
-ORDER BY user.name DESC;
+SELECT user.name  AS "User Name", COUNT(*) "Message Count" FROM user, message
+WHERE user_id = user.id
+GROUP BY user.id 
+ORDER BY user.name DESC
 
 
 -- Question: What SQL keywords or concept would you use if you wanted to automatically delete all 
 -- messages by a user if that user were deleted from the user table?
 -- Ans: Add "ON DELETE CASCADE" to the end of a target id
+
