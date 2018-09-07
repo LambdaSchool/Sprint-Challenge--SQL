08a82a49d6408034627e41bf3404f9fc940a478c
holly-g <hollygiang@penguin>
2018-09-07 17:34:14 +0000

    Insert queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE organization (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(128) NOT NULL UNIQUE,
@@ -15,6 +17,7 @@ CREATE TABLE user (
     name VARCHAR(128) NOT NULL UNIQUE
 );
 
+
 CREATE TABLE message (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     content TEXT,
@@ -23,8 +26,36 @@ CREATE TABLE message (
     channel_id INTEGER REFERENCES channel(id)
 );
 
+--- Join table for users + channels --- 
+
 CREATE TABLE user_channel (
     user_id INTEGER REFERENCES user(id),
     channel_id INTEGER REFERENCES channel(id)
 );
 
+
+
+INSERT INTO organization (name) VALUES ('Lambda School');
+
+INSERT INTO user (name) VALUES ('ALice');
+INSERT INTO user (name) VALUES ('Bob');
+INSERT INTO user (name) VALUES ('Chris');
+
+INSERT INTO channel (name, organization_id) VALUES ('#general', 1);
+INSERT INTO channel (name, organization_id) VALUES ('#random', 2);
+
+INSERT INTO user_channel (user_id, channel_id) VALUES (1, 1);
+INSERT INTO user_channel (user_id, channel_id) VALUES (1, 2);
+INSERT INTO user_channel (user_id, channel_id) VALUES (2, 1);
+INSERT INTO user_channel (user_id, channel_id) VALUES (2, 3);
+
+INSERT INTO message (content, user_id, channel_id) VALUES ('Have a great weekend!', 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES (':partypotato:', 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Ready for Lambda Labs', 1, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES ('SQL is too verbose for me', 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Happy Friday!', 2, 1);
+INSERT INTO message (content, user_id, channel_id) VALUES (':ultrafastparrot:', 2, 1 );
+INSERT INTO message (content, user_id, channel_id) VALUES (':coffeeparrot:', 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('What is the weekly retro topic?', 3, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES (':lambdareps:', 1, 2);
+INSERT INTO message (content, user_id, channel_id) VALUES ('Almost done!', 2, 1);
